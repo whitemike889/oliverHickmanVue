c884c46e53f7d9e90b755c7387f2e03fffe045a3
okhick <okhick@gmail.com>
2019-01-15 23:02:36 -0500

    Header and Router view working.

---
+++ src/components/navBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="navBar">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<·script>
+export default {
+  // aparently nothing needs to go here?
+};
+</script>
+
+<style>
+#navBar {
+  padding: 30px;
+}
+
+#navBar a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#navBar a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
